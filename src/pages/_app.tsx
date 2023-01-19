0e055d4376abb8f512cd83350ca494865da726b1
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2023-01-20 02:04:15 +0600

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import { Poppins } from '@next/font/google'
 import type { AppProps } from 'next/app'
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ['latin'],
+})
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
